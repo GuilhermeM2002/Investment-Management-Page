cd366dea755592bd75ab153b3115e83e53041304
GuilhermeM2002 <guilherme.infor2@gmail.com>
2025-09-04 17:10:49 -0300

    feat: add import files section to dashboard component

--- investment-management/src/app/components/dashboard/dashboard.component.ts
+++ investment-management/src/app/components/dashboard/dashboard.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { PortfolioSumaryComponent } from "../portfolio-sumary/portfolio-sumary.component";
 import { AddInvestmentButtonComponent } from "../add-investment-button/add-investment-button.component";
 import { LastInvestmentComponent } from "../last-investment/last-investment.component";
+import { CardComponent } from "../card/card.component";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [PortfolioSumaryComponent, AddInvestmentButtonComponent, LastInvestmentComponent],
+  imports: [PortfolioSumaryComponent, AddInvestmentButtonComponent, LastInvestmentComponent, CardComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
